f34722d0729897df4da802c9e6f220f008da83d2
Cassidy Williams <cassidoo@gmail.com>
2015-01-11 22:30:05 -0500

    Add book list
    Still doesn’t work yet…hmm.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+require 'books'
+
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
@@ -5,4 +7,15 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :todoo
+
+  after_create :init
+
+  def init
+    BOOKS.each do |item|
+      @t = Todoo.new(name: item, done: false)
+      #Make this work.
+      #@t.user = self
+      #@t.save()
+    end
+  end
 end
