8fce68de38d6a212b8cd50d3a24a7cb6c5dc1e23
Cassidy Williams <cassidoo@gmail.com>
2015-01-17 18:52:52 -0500

    Finish up backend functionality

--- app/controllers/todoo_controller.rb
+++ app/controllers/todoo_controller.rb
@@ -16,12 +16,10 @@ class TodooController < ApplicationController
   end
 
   def create
-    puts "-----------------------#{todo_params}-----------------------"
-    puts "--------------------#{@todos}--------------------"
     @todo = Todoo.new(todo_params)
     @todo.user = current_user
     if @todo.save
-      redirect_to todoo_index_path, :notice => "Your todo item was created!"
+      redirect_to todoo_index_path, :notice => "Your book was created!"
     else
       render "new"
     end
@@ -30,16 +28,21 @@ class TodooController < ApplicationController
   def update
     @todo = Todoo.find(params[:id])
     if @todo.update_attribute(:done, true)
-      redirect_to todoo_index_path, :notice => "Your todo item was marked as done!"
+      redirect_to todoo_index_path, :notice => "Your book was marked as finished!"
     else
-      redirect_to todoo_index_path, :notice => "Your todo item was unable to be marked as done!"
+      redirect_to todoo_index_path, :notice => "Your book was unable to be finished for some reason!"
     end
   end
 
   def destroy
     @todo = Todoo.find(params[:id])
-    @todo.destroy
-    redirect_to todoo_index_path, :notice => "Your todo item is deleted"
+    if @todo.update_attribute(:done, false)
+      redirect_to todoo_index_path, :notice => "You never finished that book. Liar."
+    else
+      redirect_to todoo_index_path, :notice => "What what!"
+    end
+    #@todo.destroy
+    #redirect_to todoo_index_path, :notice => "Your todo item is deleted"
   end
 
   def todo_params
